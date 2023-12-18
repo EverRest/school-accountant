7213257c96a3a8ff9a6df7a8164ac0b731b429c4
EverRest <medynskyypavlo@gmail.com>
2023-12-18 15:22:44 +0100

    added UserService.php, PaymentService.php, RoleSeeder

--- database/seeders/RolesSeeder.php
+++ database/seeders/RolesSeeder.php
@@ -3,14 +3,27 @@ declare(strict_types=1);
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
 
 class RolesSeeder extends Seeder
 {
+    private const ROLES = [
+        'owner',
+        'administrator',
+        'teacher',
+        'student',
+    ];
+
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        //
+        foreach (self::ROLES as $role) {
+            Role::firstOrCreate([
+                'name' => $role,
+                'guard_name' => 'web',
+            ]);
+        }
     }
 }
