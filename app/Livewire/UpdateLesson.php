01c90e74751c37b9ddfe2df019fad8df65c8538f
EverRest <medynskyypavlo@gmail.com>
2023-12-20 22:30:23 +0100

    added lessons

---
+++ app/Livewire/UpdateLesson.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class UpdateLesson extends Component
+{
+    public function render()
+    {
+        return view('livewire.update-lesson');
+    }
+}
