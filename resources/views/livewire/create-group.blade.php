1886b69abc6cca49e10b51c1167c223234dd2376
EverRest <medynskyypavlo@gmail.com>
2023-12-24 15:33:48 +0100

    added default buttons

--- resources/views/livewire/create-group.blade.php
+++ resources/views/livewire/create-group.blade.php
@@ -1,33 +1,50 @@
 <div class="d-flex justify-content-center align-items-center vh-300 pt-2.5">
-    <div class="card w-8/12 mx-12">
-        <div class="card-body">
-            <x-form wire:submit.prevent="submit">
-                <x-input wire:model="name" label="Name" type="name" class="form-control" id="name"
-                         placeholder="Enter name"/>
+    <p class="card w-8/12 mx-12">
+    <p class="card-body">
+        <x-form wire:submit.prevent="submit">
+            <x-input wire:model="name" label="Name" type="name" class="form-control" id="name"
+                     placeholder="Enter name"/>
+            @if(!$students->empty())
                 <x-select wire:model="selectedCourse" label="Select Courses" icon="o-pencil" :options="$courses"
                           inline/>
-                @if($students)
-                    <x-choices wire:model="selectedStudents" icon="o-users" hint="Add student to the course"
-                               label="Students" :options="$students"/>
-                @endif
-                @if($teachers)
-                    <x-choices wire:model="selectedTeachers" icon="o-user-circle" hint="Add teacher to the course"
-                               label="Teachers" :options="$teachers"/>
-                @endif
-                @if (session()->has('error'))
-                    <x-alert icon="o-exclamation-triangle" class="alert-danger">
-                        {{ session('error') }}
-                    </x-alert>
-                @endif
-                <x-slot:actions>
-                    <x-button label="Create Group" class="btn-primary" type="submit" spinner="save"/>
-                </x-slot:actions>
-            </x-form>
-            @if (session()->has('message'))
-                <x-alert type="success">
-                    {{ session('message') }}
+            @else
+                <div class="text-center">
+                    <x-button label="Add course" icon="o-pencil" class="btn-active btn-sm w-1/3"
+                              link="{{route('courses.create')}}"/>
+                </div>
+            @endif
+            @if(!$students->empty())
+                <x-choices wire:model="selectedStudents" icon="o-users" hint="Add student to the course"
+                           label="Students" :options="$students"/>
+            @else
+                <div class="text-center">
+                    <x-button label="Add student" icon="o-users" class="btn-accent btn-sm w-1/3"
+                              link="{{route('students.create')}}"/>
+                </div>
+            @endif
+            @if(!$teachers->empty())
+                <x-choices wire:model="selectedTeachers" icon="o-user-circle" hint="Add teacher to the course"
+                           label="Teachers" :options="$teachers"/>
+            @else
+                <div class="text-center">
+                    <x-button label="Add teacher" icon="o-user-circle" class="btn-primary btn-sm w-1/3"
+                              link="{{route('teachers.create')}}"/>
+                </div>
+            @endif
+            @if (session()->has('error'))
+                <x-alert icon="o-exclamation-triangle" class="alert-danger">
+                    {{ session('error') }}
                 </x-alert>
             @endif
-        </div>
-    </div>
+            <x-slot:actions>
+                <x-button label="Create Group" class="btn-primary" type="submit" spinner="save"/>
+            </x-slot:actions>
+        </x-form>
+        @if (session()->has('message'))
+            <x-alert type="success">
+                {{ session('message') }}
+            </x-alert>
+    @endif
+</div>
+</div>
 </div>
