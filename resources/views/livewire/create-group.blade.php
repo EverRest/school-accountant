52d23b1f03092cf58d276fe967c07d280f24e459
EverRest <medynskyypavlo@gmail.com>
2023-12-25 09:18:34 +0100

    test-2

--- resources/views/livewire/create-group.blade.php
+++ resources/views/livewire/create-group.blade.php
@@ -11,8 +11,8 @@
                 wire:model="selectedCourse"
                 single/>
             @if($students->isNotEmpty())
-{{--                <x-choices wire:model="selectedStudents" icon="o-users" hint="Add student to the course"--}}
-{{--                           label="Students" :options="$students"/>--}}
+                <x-choices wire:model="selectedStudents" icon="o-users" hint="Add student to the course"
+                           label="Students" :options="$students"/>
             @else
                 <div class="text-center">
                     <x-button label="Add student" icon="o-users" class="btn-accent btn-sm w-1/3"
@@ -20,8 +20,8 @@
                 </div>
             @endif
             @if($teachers->isNotEmpty())
-                <x-choices wire:model="selectedTeacher" icon="o-user-circle" hint="Add teacher to the course"
-                           label="Teachers" :options="$teachers" single/>
+{{--                <x-choices wire:model="selectedTeacher" icon="o-user-circle" hint="Add teacher to the course"--}}
+{{--                           label="Teachers" :options="$teachers" single/>--}}
             @else
                 <div class="text-center">
                     <x-button label="Add teacher" icon="o-user-circle" class="btn-primary btn-sm w-1/3"
